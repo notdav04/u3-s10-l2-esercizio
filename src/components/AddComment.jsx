da1951dd67754f6c3fb0c3f8b88af9ab6ca1f93d
notdav04 <davide.alonzi26@gmail.com>
2024-12-03 15:29:51 +0100

    Svolgimento Esercizio

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,26 +1,20 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class AddComment extends Component {
-  state = {
-    obj: {
-      comment: "",
-      rate: "",
-      author: "",
-      elementId: this.props.idLibro
-    }
-  };
+const AddComment = (props) => {
+  const [obj, setObj] = useState({
+    comment: "",
+    rate: "",
+    author: "",
+    elementId: props.idLibro
+  });
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState(() => ({
-      obj: {
-        ...this.state.obj,
-        [name]: value
-      }
-    }));
+
+    setObj((prevObj) => ({ ...prevObj, [name]: value }));
   };
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const url = "https://striveschool-api.herokuapp.com/api/comments/";
     try {
@@ -31,15 +25,16 @@ class AddComment extends Component {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTY2NzhhZDEyOTAwMTU4NzZiZTEiLCJpYXQiOjE3MzI4MDI4MzEsImV4cCI6MTczNDAxMjQzMX0.4Wo_n0XxH834Cma8Z6xwatgoMVzlxcp54ZMQwtcKZ9M"
         },
-        body: JSON.stringify(this.state.obj)
+        body: JSON.stringify(obj)
       });
 
       if (response.ok) {
         alert("Commento aggiunto con successo!");
-        this.setState({ obj: { comment: "", rate: "", author: "" } });
+
+        setObj({ elementId: props.idLibro, comment: "", rate: "", author: "" });
       } else {
         console.log(response);
-        console.log(this.state.obj);
+        console.log(obj);
         alert("Errore nell'invio del commento." + response.statusText);
       }
     } catch (error) {
@@ -47,48 +42,46 @@ class AddComment extends Component {
     }
   };
 
-  render() {
-    return (
-      <div>
-        <h3>Aggiungi un Commento</h3>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Commento:</label>
-            <textarea
-              name="comment"
-              value={this.state.obj.comment}
-              onChange={this.handleChange}
-              rows="3"
-              required
-            />
-          </div>
-          <div>
-            <label>Voto (1-5):</label>
-            <input
-              type="number"
-              name="rate"
-              value={this.state.obj.rate}
-              onChange={this.handleChange}
-              min="1"
-              max="5"
-              required
-            />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="author"
-              value={this.state.obj.author}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Invia Commento</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h3>Aggiungi un Commento</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Commento:</label>
+          <textarea
+            name="comment"
+            value={obj.comment}
+            onChange={handleChange}
+            rows="3"
+            required
+          />
+        </div>
+        <div>
+          <label>Voto (1-5):</label>
+          <input
+            type="number"
+            name="rate"
+            value={obj.rate}
+            onChange={handleChange}
+            min="1"
+            max="5"
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="author"
+            value={obj.author}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Invia Commento</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddComment;
